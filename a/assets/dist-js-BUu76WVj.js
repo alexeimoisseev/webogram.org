eb80ddae6a7dc8704e3eb8d92be9df6400a81e93
Alexei Moisseev <alexmoiss@gmail.com>
2026-07-15 14:01:10 +0300

    Telegram Web upgrade

---
+++ a/assets/dist-js-BUu76WVj.js
@@ -0,0 +1,2 @@
+import{Ov as e}from"./shared-components-DfVMMrka.js";async function t(){await e(`plugin:process|restart`)}export{t as relaunch};
+//# sourceMappingURL=dist-js-BUu76WVj.js.map
--- a/assets/dist-js-BYQsacxt.js
+++
@@ -1,2 +0,0 @@
-import{yv as e}from"./shared-components-Mv8vLbE2.js";async function t(){await e(`plugin:process|restart`)}export{t as relaunch};
-//# sourceMappingURL=dist-js-BYQsacxt.js.map
